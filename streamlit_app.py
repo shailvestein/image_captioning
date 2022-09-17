452938ac1ed590844a51df96846ff8c8ca39f459
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-09-18 01:41:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import numpy as np
 import pickle as pkl
 import os
 import time
-# from tensorflow.keras.layers import Input, Embedding, LSTM, Dense, Dropout, Add, Concatenate
-# from tensorflow.keras.models import Model
-# import tensorflow as tf
+from tensorflow.keras.layers import Input, Embedding, RepeatVector, TimeDistributed, LSTM, Dense, Dropout, Add, Concatenate
+from tensorflow.keras.models import Model
+import tensorflow as tf
 
 from tensorflow import expand_dims
 from tensorflow.keras.applications.efficientnet import EfficientNetB7, preprocess_input
@@ -15,18 +15,51 @@ from tensorflow.keras.utils import load_img, img_to_array
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 SHAPE=(600,600)
-max_length=33
+MAX_LENGTH=33
+
+
+@st.cache
+def load_tokenizer():
+    with open('tokenizer.pkl', 'rb') as f:
+        tokenizer = pkl.load(f)
+    return tokenizer 
+    
+tokenizer = load_tokenizer()
+inverse_vocabulary = tokenizer.index_word
+vocab_size=len(inverse_vocabulary)+1
 
 @st.cache
 def load_feature_extractor():
     # downloading tensorflow pre-trained model for generating feature from image 
+    # url = 'https://drive.google.com/uc?id=1-050q5AQWBArHiDZf6yRH6bXk29KoWPV'
+    # output = 'feature_extractor.h5'
     efficientnet_b7=EfficientNetB7(weights='imagenet')
     feature_extractor=Model(inputs=efficientnet_b7.inputs, outputs=efficientnet_b7.layers[-2].output)
-    # feature_extractor.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+    optmizer = tf.keras.optimizers.Adam(1e-5)
+    feature_extractor.compile(optimizer=optmizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    
     return feature_extractor
     
     
 feature_extractor = load_feature_extractor()
+
+@st.cache
+def build_model(feature_input_shape, vocab_size, units, max_length, embedding_dim):
+    input1 = Input(shape=feature_input_shape, name='feature_input_layer')
+    x1=Dense(units, activation='relu', name='dense_layer_1_after_feature_input')(input1)
+    x1=RepeatVector(max_length, name='repeat_vector_layer')(x1)
+    input2 = Input(shape=(max_length,), name='caption_input_layer')
+    x2=Embedding(vocab_size, embedding_dim, input_length=max_length, mask_zero=True, trainable=False, name='Embedding_layer')(input2)
+    x2=LSTM(units, return_sequences=True, name='lstm_layer_1')(x2)
+    x2=TimeDistributed(Dense(embedding_dim, name='densly_time_distributed_layer'), name='time_distributed_layer')(x2)
+
+    x=Concatenate(name='concate_layer')([x1,x2])
+    x=LSTM(units, return_sequences=True)(x)
+    x=LSTM(units)(x)
+    x=Dense(units, activation='relu', name='dense_layer_1_after_concat')(x)
+    output=Dense(vocab_size, activation='softmax', name='output_layer')(x)
+    model=Model(inputs=[input1, input2], outputs=output, name='image_captioning_model')
+    return model
 
 @st.cache
 def load_caption_generator():
@@ -34,20 +67,14 @@ def load_caption_generator():
     url = "https://drive.google.com/uc?id=100gJejr3YcYKHQGWZd7WGSLdeprPiuEG"
     output="image_captioner.h5"
     gdown.download(url, output, quiet=False)
-    time.sleep(2)
-    caption_generator = "image_captioner.h5"
+    time.sleep(0.5)
+    caption_generator = build_model(feature_input_shape=2560, vocab_size=vocab_size, units=512, max_length=MAX_LENGTH, embedding_dim=EMBEDDING_DIM)
+    caption_generator.load_weights("image_captioner.h5")
     return caption_generator
     
 caption_generator = load_caption_generator()
 
-@st.cache
-def load_tokenizer():
-    with open('tokenizer.pkl', 'rb') as f:
-        tokenizer = pkl.load(f)
 
-    return tokenizer 
-    
-tokenizer = load_tokenizer()
 
 # title for the webpage
 st.title("Image Captioning Using Deep Learning")
@@ -100,7 +127,7 @@ if submitted:
             # getting index of predicted word 
             word_index = np.argmax(yhat, axis=-1)[0]
             # getting word for index from inverse vocabulary
-            word = inverse_vocabulary.get(word_index)
+            word = tokenizer.index_word.get(word_index)
             # concatenating generated word to result
             result += ' ' + word 
             # breaking loop/prediction if <end> word detected
