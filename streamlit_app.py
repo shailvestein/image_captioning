da85b5985b3958c37fdf072de82fb6567a11a905
shailvestein <62176093+shailvestein@users.noreply.github.com>
2022-09-20 22:02:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,27 @@ import pickle as pkl
 import os
 from PIL import Image
 
-from tensorflow.keras.layers import Input, Embedding, RepeatVector, GRU, TimeDistributed, LSTM, Dense, Dropout, Concatenate
+from tensorflow.keras.layers import Input, Embedding, RepeatVector, TimeDistributed, LSTM, Dense, Dropout, Concatenate
 from tensorflow.keras.models import Model
 import tensorflow as tf
 
 from tensorflow import expand_dims
-from tensorflow.keras.applications.resnet import ResNet50, preprocess_input
+from tensorflow.keras.applications.efficientnet import EfficientNetB7, preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-SHAPE=(224,224)
-MAX_LENGTH=33
+SHAPE=(600,600)
+MAX_LENGTH=30
 EMBEDDING_DIM=128
 
 
-@st.cache
+@st.cache(max_entries=1)
 def load_tokenizer():
-    with open('tokenizer.pkl', 'rb') as f:
+    tokenizer_url = "https://drive.google.com/uc?id=1-B6QUbYBUmA9Zc7lFucwH8LgVW4oB1r0"
+    tokenizer_output = "tokenizer.pkl"
+    gdown.download(tokenizer_url, tokenizer_output)
+    with open('./tokenizer.pkl', 'rb') as f:
         tokenizer = pkl.load(f)
     return tokenizer 
     
@@ -32,16 +35,16 @@ vocab_size=len(inverse_vocabulary)+1
 
 @st.cache(max_entries=1)
 def load_feature_extractor():
-    # downloading tensorflow pre-trained model for generating feature from image 
-    resnet50=ResNet50(weights='imagenet')
-    feature_extractor=Model(inputs=resnet50.inputs, outputs=resnet50.layers[-2].output)
-    # optmizer = tf.keras.optimizers.Adam(1e-2)
-    # feature_extractor.compile(optimizer=optmizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    print(f"Downloading feature extractor from tensorflow...", end="")
+    efficientnet=EfficientNetB7(weights='imagenet')
+    feature_extractor=Model(inputs=efficientnet.inputs, outputs=efficientnet.layers[-2].output)  
+    print(f"done!")
     return feature_extractor
-
 feature_extractor = load_feature_extractor()
 
-def build_seq2seq_model(feature_input_shape, rate, vocab_size, embedding_dim, max_length, ):
+@st.cache(max_entries=1)
+def build_seq2seq_model(feature_input_shape=2560, rate=0.2, vocab_size=vocab_size, embedding_dim=EMBEDDING_DIM, max_length=max_length):
+    tf.keras.backend.clear_session()
     input_1 = Input(shape=(feature_input_shape,), name='input_1_layer')
     x1 = Dense(embedding_dim, activation='relu', name='input_1_dense_1_layer')(input_1)
     x1 = Dropout(rate, name='input_1_dropout_layer')(x1)
@@ -49,24 +52,25 @@ def build_seq2seq_model(feature_input_shape, rate, vocab_size, embedding_dim, ma
 
     input_2 = Input(shape=(max_length,), name='input_2_layer')
     x2 = Embedding(vocab_size, embedding_dim, input_length=max_length, mask_zero=True, name='embedding_layer_1')(input_2)
-    x2 = LSTM(256, return_sequences=True, name='input_2_layer_LSTM_1')(x2)
+    x2 = LSTM(int(embedding_dim*2), return_sequences=True, name='input_2_layer_LSTM_1', dropout=0.2)(x2)
     x2 = TimeDistributed(Dense(embedding_dim, activation='softmax', name='dec_time_distributed_layer'), name='time_dsitributed_layer')(x2)
     x = Concatenate()([x1,x2])  
     
-    x = LSTM(128, return_sequences=True, name='decoder_layer_LSTM_1')(x)
-    x = LSTM(512, return_sequences=False, name='decoder_layer_LSTM_2')(x)
+    x = LSTM(embedding_dim, return_sequences=True, name='decoder_layer_LSTM_1', dropout=0.2)(x)
+    x = LSTM(int(embedding_dim*4), return_sequences=False, name='decoder_layer_LSTM_2', dropout=0.2)(x)
 
     output = Dense(vocab_size, activation='softmax', name='dec_output_layer')(x)
     model = Model(inputs=[input_1, input_2], outputs=output, name='seq2seq_model')
+
     return model
 
-@st.cache
+@st.cache(max_entries=1)
 def load_caption_generator():
     # downloading trained caption generator model from my google drive 
-    # url = "https://drive.google.com/uc?id=10AkZ2UTReklr_lDlJ1R8jUWszr_OCscG"
+    url = "https://drive.google.com/uc?id=1-DwVRfkqnhEHaLA0HBqdWVo-TY4KwhB0"
     output="image_captioner.h5"
-    # gdown.download(url, output, quiet=False)
-    caption_generator = build_seq2seq_model(feature_input_shape=2048, rate=0.2, vocab_size=vocab_size, embedding_dim=EMBEDDING_DIM, max_length=MAX_LENGTH)
+    gdown.download(url, output, quiet=False)
+    caption_generator = build_seq2seq_model()
     caption_generator.load_weights(output)
     return caption_generator
     
